f3795e2e4b5fcc80f5d1416851f8c93cd987bfd8
Athul Sathya <sathyaathul92@gmail.com>
2026-05-16 22:27:03 +0530

    component updated

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const apiURL=import.meta.env.VITE_API_URL
+
 const Api = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: apiURL,
   withCredentials: true,
 });
 
